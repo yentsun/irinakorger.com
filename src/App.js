d733081505e0dd56c08551b053c8226ab56455de
Maksim Korinets <mkorinets@gmail.com>
2023-08-14 11:31:47 +0700

    update deploy script

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo-clean.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Coming soon...
+          🛠️ Coming soon...
         </p>
       </header>
     </div>
