3dc9030f4ecbc37486d63bcb5fb03c0b261a1e27
Maksim Korinets <mkorinets@gmail.com>
2023-08-18 11:07:18 +0700

    minor changes

---
+++ src/Base.js
@@ -0,0 +1,24 @@
+import logo from './logo-clean.svg';
+
+
+function Base() {
+
+
+    return <>
+
+        <header>
+
+            <nav>
+
+            </nav>
+
+        </header>
+
+        <section id="home">
+
+        </section>
+
+    </>;
+}
+
+export default Base;
